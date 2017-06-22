c3e68c17e57517f7e9313e1f30a045cc398919d2
Ramnivas Murugesan <mramnivas@gmail.com>
2017-06-22 23:26:49 +0530

    Init Setup for development

--- Application/public/webapp/js/Controllers/userController.js
+++ Application/public/webapp/js/Controllers/userController.js
@@ -19,184 +19,7 @@ FHMOD.controller("ManageUsersModels",function($scope,$uibModalInstance,items){
 	switch(items.option){
 
 		case 'crud.new':
-		case 'crud.edit':
-
-				$scope.passwordDisabled = false;
-
-				$scope.Error = ""; $scope.loadingMsg = "";
-
-				$scope.model = {};
-
-				$scope.companies = []; 
-
-				$scope.hubs = []; 
-
-				$scope.CompaniesAndHubs = [];
-
-				var Error = {}; 
-
-				var Loading = {};
-
-				$scope.filterHubs = function(){
-
-					$scope.model.frmUserHub = "0";
-					$scope.hubs = new jsonFinder($scope.CompaniesAndHubs).findAll("parent_id").val($scope.model.frmUserCompany).data ;
-		 			 
-				}
-
-				Error.set = function(data){   
-
-						$scope.errorDisplay = "block"; 
-						$scope.Error = data ; 
-
-				}
-
-				Error.off = function(){
-
-						$scope.errorDisplay = "none";
-						$scope.Error = "" ; 
-
-				}
-
-				Loading.set = function(data){   
-
-						$scope.loadingDisplay = "block";
-						$scope.loadingMsg = data ; 
-
-				}
-
-				Loading.off = function(){
-
-						$scope.loadingDisplay = "none";
-						$scope.loadingMsg = "" ; 
-
-				}
-
-				$scope.loadingDisplay = $scope.errorDisplay = "none";
-
-				$scope.roles = [];
-
-				$scope.clear = function(){
-
-					if(items.data){
-
-						$scope.passwordDisabled = true;
-
-						$scope.model.frmUserName = items.data.username;
-						$scope.model.frmUserPassword= "";
-						$scope.model.frmUserConfirmPassword="";
-						$scope.model.frmUserEmail =items.data.email;
-						$scope.model.frmUserStatus=String(items.data.status);
-						
-
-
-						//$scope.model.frmUserCompany=items.data.company_id;
-						//$scope.model.frmUserHub="0"; 
-						//$scope.model.frmUserRole=items.data.user_role;						
-
-					}	
-					else{
-
-						$scope.model.frmUserName = "";
-						$scope.model.frmUserPassword="";
-						$scope.model.frmUserConfirmPassword="";
-						$scope.model.frmUserEmail ="";
-						$scope.model.frmUserStatus="";
-						$scope.model.frmUserCompany="0";
-						$scope.model.frmUserHub="0"; 
-						$scope.model.frmUserRole=""; 
-
-					}
-
-					
-				}
-
-				$scope.clear();
-
-				$scope.save = function(){
-
-					Loading.set("Saving...");
-
-					data = {
-							
-							id: (items.data ? (items.data.id ): ("0") ),
-							username:$scope.model.frmUserName,
-							password:$scope.model.frmUserPassword,
-							confirm_password:$scope.model.frmUserConfirmPassword,
-							email:$scope.model.frmUserEmail , 
-							status:$scope.model.frmUserStatus,
-							company_id:$scope.model.frmUserCompany, 
-							hub_id:$scope.model.frmUserHub, 
-							user_role:$scope.model.frmUserRole
-
-					};
-		 
-					Users.save(data).success(function(_data){
-
-						Loading.off();
-
-						Error.off();
-
-						var res = new Response(_data);  
-
-						if(res.isOk()){
-
-							$uibModalInstance.close("save");
-
-						}
-						else{
-
-							Error.set(res.Msg());
-
-						}
-
-					});
-
-				}
-
-				$scope.Disabled = {
-
-					CompanyDisabled:false,
-					HubDisabled:false
-
-				};
-		 
-		 
-
-				$scope.rolesChange = function(){
-
-
-					makeDisabledFields();
-
-					 
-				}
-
-				var makeDisabledFields = function(){
-
-					if($scope.model.frmUserRole == 1){
-		 				
-						$scope.Disabled.CompanyDisabled = true;
-		  				$scope.Disabled.HubDisabled = true;
-
-		  				$scope.model.frmUserCompany="0"; 
-		  				$scope.model.frmUserHub="0";
-		 
-					}
-					else if( $scope.model.frmUserRole == 3 ){
-
-						$scope.Disabled.CompanyDisabled = false;
-		  				$scope.Disabled.HubDisabled = true;
-		  				$scope.model.frmUserHub="0";
-		 
-					}
-					else{
-
-						$scope.Disabled.CompanyDisabled = false;
-		  				$scope.Disabled.HubDisabled = false;
-
-					}
-
-				}
+		case 'crud.edit': 
 				
 
 		break;
@@ -216,20 +39,13 @@ FHMOD.controller("ManageUsersModels",function($scope,$uibModalInstance,items){
 //Controller
 FHMOD.controller("ManageUsers",function($scope,$uibModal){
 
- 		$scope.companies = $scope.CompaniesAndHubs = {};
 
-		$scope.filteredByCompany = "0";
-
-		$scope.filteredByRole = "0";
-
-		$scope.userGrid = [];
-
-		$scope.roles = [];
 
 
 		$scope.crud = {
 
 			new: function(){
+
 				var ModalWindow_New = $uibModal.open({
 
 					templateUrl : 'form.html',
@@ -276,48 +92,36 @@ FHMOD.controller("ManageUsers",function($scope,$uibModal){
 			},
   
 			delete : function(id){
+ 
 
-				 var deleteRow = new jsonFinder($scope.userGrid).findby("id").val(id) ;
+				 var ModalWindow_Delete = $uibModal.open({
 
-				 if(deleteRow.result){
-
-					 var ModalWindow_Delete = $uibModal.open({
-
-						templateUrl : 'alert.html',
-						controller: 'ManageUsersModels',
-						resolve: {
-							items:function(){
-								return {
-									option:'crud.delete',
-									title:'Delete Confirmation "'+ deleteRow.data.data.username +'"'
-								}
+					templateUrl : 'alert.html',
+					controller: 'ManageUsersModels',
+					resolve: {
+						items:function(){
+							return {
+								option:'crud.delete',
+								title:'Delete Confirmation "'+ deleteRow.data.data.username +'"'
 							}
 						}
+					}
 
-					 });
+				 });
 
-					 ModalWindow_Delete.result.then(function(opt){
+				 ModalWindow_Delete.result.then(function(opt){
 
-				    	 if(opt == "ok"){
- 
-				    	 	 Users.deleteUser(deleteRow.data.data.id).success(function(_data){
+			    	 if(opt == "ok"){
 
-				    	 	 		 if(_data.status == "done"){
+			    	 	  
+			    	 }
 
-				    	 	 		 	 $scope.userGrid.splice(deleteRow.data.index,1);
+			     },function(){});
 
-				    	 	 		 }
-
-				    	 	 })
-
-				    	 }
-
-				     },function(){});
-
-
-				}
 
 			}
+
+			 
 
 		}
 
